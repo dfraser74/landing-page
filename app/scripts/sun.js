c9838369febf47c4f6dc9df776958236779ae7c8
Denis Baskovsky <denis@baskovsky.ru>
2016-07-31 20:45:44 +0300

    Изменен стиль логотипа, Правки верстки, Оптимизация скорости

--- app/scripts/sun.js
+++ app/scripts/sun.js
@@ -1,15 +1,16 @@
 const mainBlock = document.querySelector('.text-logo');
 var mouse = {x: 0, y: 0};
+var mainBlockClientRect = mainBlock.getBoundingClientRect();
 var wind = {
-  w: window.innerWidth / 2,
-  h: 100 + (mainBlock.offsetHeight / 2)
+  w: (mainBlockClientRect.left + mainBlockClientRect.width / 2),
+  h: 0
 };
 
-var sunTween = TweenMax.fromTo(mouse, 3, {x: mainBlock.offsetWidth / 2, y: -100}, {
+var sunTween = TweenMax.fromTo(mouse, 3, {x: mainBlock.offsetWidth / 2, y: 0}, {
   yoyo: true,
   repeat: -1,
-  x: mainBlock.offsetWidth / 2,
-  y: -500,
+  x: 0,
+  y: 0,
   ease: Power3.easeInOut
 });
 
@@ -26,13 +27,14 @@ function onMouseMove(e) {
 }
 
 function ontick() {
-  wind.w = window.innerWidth / 2;
   let shadow = '';
-  let i = 20;
+  let i = 7;
   let len = i;
 
   while (i--) {
-    shadow += ( Math.round(i * -((mouse.x - (wind.w)) / 100)) + 'px ' + Math.round(i * -((mouse.y - wind.h) / 100)) + 'px ' + (i * 4) + 'px hsla(255%,255%,255%,' + 1 / (len - 1) + '),' );
+    shadow += ( Math.round(i * -((mouse.x - (wind.w)) / 100)) + 'px ' +
+    Math.round(i * -((mouse.y - wind.h) / 100)) + 'px ' +
+    (i * 6) + 'px hsla(255%,255%,255%,' + 1 / (len - 1) + '),' );
   }
 
   TweenMax.set(mainBlock, {textShadow: shadow});
