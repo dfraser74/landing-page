0791cc08591ae164305d59f9374ff1c17172e651
Denis Baskovsky <denis@baskovsky.ru>
2016-10-13 22:51:30 +0300

    Правка стилей, Обновлены и добавлены новые картинки, Добавлены ссылки на соц сети, Изменена верстка в Subscribe

--- app/scripts/slides.js
+++ app/scripts/slides.js
@@ -1,64 +1,27 @@
 (function () {
   'use strict';
 
-  [].forEach.call(document.querySelectorAll('.iframe-container'), elem => {
+  Array.from(document.querySelectorAll('.iframe-container'), elem => {
     elem.onclick = (e) => {
       const iframe = e.target.querySelector('iframe');
+
       if (iframe) {
         iframe.classList.remove('no-select');
       }
+
     };
   });
 
-  new Swiper('.section1 .swiper-container', {
-    pagination: '.swiper-pagination',
-    slidesPerView: 1,
-    centeredSlides: true,
-    paginationClickable: true,
-    spaceBetween: 30,
-    loop: false,
-    grabCursor: true
-  });
-
-// swiper 1
-  new Swiper('.section2 .swiper-container', {
-    pagination: '.swiper-pagination',
-    slidesPerView: 1,
-    centeredSlides: true,
-    paginationClickable: true,
-    spaceBetween: 30,
-    loop: true,
-    grabCursor: true
-  });
-
-  new Swiper('.section3 .swiper-container', {
-    pagination: '.swiper-pagination',
-    slidesPerView: 1,
-    centeredSlides: true,
-    paginationClickable: true,
-    spaceBetween: 30,
-    loop: true,
-    grabCursor: true
-  });
-
-  new Swiper('.section4 .swiper-container', {
-    pagination: '.swiper-pagination',
-    slidesPerView: 1,
-    centeredSlides: true,
-    paginationClickable: true,
-    spaceBetween: 30,
-    loop: true,
-    grabCursor: true
-  });
-
-  new Swiper('.section5 .swiper-container', {
-    pagination: '.swiper-pagination',
-    slidesPerView: 1,
-    centeredSlides: true,
-    paginationClickable: true,
-    spaceBetween: 30,
-    loop: true,
-    grabCursor: true
+  Array.from(document.querySelectorAll('.swiper-container'), swiperContainer => {
+    new Swiper(swiperContainer, {
+      pagination: '.swiper-pagination',
+      slidesPerView: 1,
+      centeredSlides: true,
+      paginationClickable: true,
+      spaceBetween: 30,
+      loop: false,
+      grabCursor: true
+    });
   });
 
 }());
