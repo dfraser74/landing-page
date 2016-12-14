c566deb06d361ca663df477bfc9d8165051b8c38
Denis Baskovsky <denis@baskovsky.ru>
2016-12-14 22:36:30 +0300

    Swiper Scrollbar change background, Serve of https, gulp-sass updated,

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -130,7 +130,7 @@ gulp.task('serve', ['scripts', 'styles'], () => {
     // Run as an https by uncommenting 'https: true'
     // Note: this uses an unsigned certificate which on first access
     //       will present a certificate warning in the browser.
-    // https: true,
+    https: true,
     server: ['.tmp', 'app'],
     port: 3000
   });
