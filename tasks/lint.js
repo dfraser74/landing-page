ac4082496e309178db53191b3ed38e295c5f3be3
Deni Baskovsky <baskovsky@ya.ru>
2018-05-05 10:59:35 +0300

    packages optimization

--- tasks/lint.js
+++ tasks/lint.js
@@ -1,6 +1,5 @@
 'use strict';
 import gulp from 'gulp';
-import browserSync from 'browser-sync';
 import gulpLoadPlugins from 'gulp-load-plugins';
 const $ = gulpLoadPlugins();
 
@@ -9,5 +8,4 @@ gulp.task('lint', () =>
   gulp.src('app/scripts/**/*.js')
     .pipe($.eslint())
     .pipe($.eslint.format())
-    .pipe($.if(!browserSync.active, $.eslint.failOnError()))
 );
